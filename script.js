91bc92a2bd85e0e9105715769165b7a1c8ea75a4
Kacper Klimczak <kacper_klimczak_2609@protonmail.com>
2023-03-09 00:55:34 +0100

    add filter and search script

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
 const flags = document.querySelector('#flags');
-fetch('https://restcountries.com/v3.1/all')
+const filter = document.querySelector('#filter');
+const country = document.querySelector('#name');
+const options = document.querySelectorAll('option');
+    //display on start
+    fetch(`https://restcountries.com/v3.1/all`)
     .then(response => response.json())
     .then(data=>show(data))
+    //filter changes
+    filter.addEventListener('input',()=>{
+        country.value = "";
+        if(filter.value ==="all"){
+            fetch(`https://restcountries.com/v3.1/all`)
+            .then(response => response.json())
+            .then(data=>show(data))
+        }else{
+            fetch(`https://restcountries.com/v3.1/region/${filter.value}`)
+            .then(response => response.json())
+            .then(data=>show(data))
+        }
+    
+})
+    //input changes
+    country.addEventListener('input',()=>{
+        if(country.value === ''){
+            fetch(`https://restcountries.com/v3.1/all`)
+            .then(response => response.json())
+            .then(data=>show(data)) 
+        }else{
+            options[0].setAttribute('selected', true)
+            console.log(`https://restcountries.com/v3.1/name/${country.value}`)
+            fetch(`https://restcountries.com/v3.1/name/${country.value}`)
+            .then(response => response.json())
+            .then(data=>show(data))
+        }
+        
+    })
+
 
 
 function show(data) {
-    console.log(data[0])
+    flags.innerHTML = ""
     data.forEach(country =>{
         let flag = document.createElement('div');
         let countryFlag = country.flags.png;
